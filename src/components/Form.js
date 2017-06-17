97a4416ba93b46051a5527aa899ca31469da9364
limozaza <zakboite@gmail.com>
2017-06-17 15:28:06 +0200

    upsate state

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,13 +6,21 @@ class Form  extends React.Component{
     quantity: 0
   };
 
+
+
   render(){
     return(
       <div>
         <h3>Ajouter des articles à acheter</h3>
         <form>
-          <input type="number" placeholder="quantité" />
-          <input type="text" placeholder="article" />
+          <input type="number" placeholder="quantité" value={this.state.quantity}
+            onChange={
+              (e) => this.setState({quantity: e.target.value})
+            }/>
+          <input type="text" placeholder="article" value={this.state.name}
+            onChange={
+              (e) => this.setState({name: e.target.value})
+            }/>
           <button type="submit">ajouter</button>
         </form>
       </div>
