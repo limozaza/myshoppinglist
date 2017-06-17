c6309850f32bf0ffed7887139c5c8e9907e53ffd
limozaza <zakboite@gmail.com>
2017-06-17 13:21:09 +0200

    Add Formulaire in Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Form = () => {
-  return (
-    <div>
-      From component
-    </div>
-  );
-};
+class Form  extends React.Component{
+  state ={
+    name: '',
+    quantity: 0
+  };
 
+  render(){
+    return(
+      <div>
+        <h3>Ajouter des articles à acheter</h3>
+        <form>
+          <input type="number" placeholder="quantité" />
+          <input type="text" placeholder="article" />
+          <button type="submit">ajouter</button>
+        </form>
+      </div>
+    );
+  }
+}
 export default Form;
