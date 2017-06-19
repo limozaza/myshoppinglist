2a1d60190d92900481ee2d92bbbf8bfdecfc8690
limozaza <zakboite@gmail.com>
2017-06-19 16:02:54 +0200

    Add redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import {render} from 'react-dom';
 
 import App from './components/App';
 
+import { createStore, combineReducers } from 'redux';
+
+
+const store = createStore();
+
 render(
   <App/>,document.getElementById('root')
 );
